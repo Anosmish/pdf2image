3ea469473fa9a7b886c6f865cd33f30e1b04c63c
Anosmish <146194026+Anosmish@users.noreply.github.com>
2025-10-05 19:41:05 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
     formData.append('scale', document.getElementById('scale').value);
 
     try {
-      const backendUrl = 'https://your-backend.onrender.com/convert.php';
+      const backendUrl = 'https://pdf2image-gg8f.onrender.com/convert.php';
       const response = await fetch(backendUrl, { method: 'POST', body: formData });
 
       if (!response.ok) {
